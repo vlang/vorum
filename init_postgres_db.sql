4958fc3a96c1a6020af1062f431f8de09cc2bfd3
Alexander Medvednikov <alexander@medvednikov.com>
2019-07-30 00:51:21 +0200

    postgres db init sql script

---
+++ init_postgres_db.sql
@@ -0,0 +1,27 @@
+CREATE TABLE comments (
+    id serial primary key,
+    post_id integer DEFAULT 0,
+    text text DEFAULT '',
+    name text DEFAULT '',
+    "time" timestamp without time zone DEFAULT now(),
+    t integer DEFAULT date_part('epoch'::text, now())
+);
+
+
+CREATE TABLE posts (
+    id serial primary key,
+    text text DEFAULT ''::text,
+    title text DEFAULT ''::text,
+    "time" timestamp without time zone DEFAULT now(),
+    nr_comments integer DEFAULT 0,
+    is_blog boolean DEFAULT false,
+    last_reply timestamp without time zone DEFAULT now()
+);
+
+
+CREATE TABLE public.users (
+    id serial primary key,
+    name text DEFAULT ''::text,
+    random_id text DEFAULT ''::text
+);
+
